0f7b2cb23c2f7a4e7e2308e956afd6ece9e4e42f
Jiaming Xie <jmxie@pku.edu.cn>
2018-12-18 17:19:44 +0800

    pd base

--- src/pd.cc
+++ src/pd.cc
@@ -3,7 +3,31 @@
 #include <stdio.h>
 
 
-PD::PD(uint32_t sets, uint32_t prof_ratio, uint32_t prof_siz, uint32_t reuse_cnt_stp, uint32_t stge) {
+PD::PD(uint32_t prof_siz, uint32_t reuse_cnt_wid) {
+    prof_size = prof_siz;
+    reuse_cnt_width = reuse_cnt_wid;
+
+    set = LLC_SET;
+    prof_set = sets / 64;
+    stage = 512 * 1024;
+    stage_cnt = 0;
+    max_dis = 256;
+    prt_dis = max_dis;
+
+    // prof_step = 
+
+    // reuse_cnt_width = 4;
+    reuse_dis_cnt = new uint32_t[max_dis / reuse_cnt_width];
+
+    remain_prt_dis = new uint32_t* [prof_set];
+    for (int i = 0; i < prof_set; ++i) {
+        remain_prt_dis[i] = new uint32_t[LLC_WAY];
+        memset(remain_prt_dis[i], 0, sizeof(uint32_t) * LLC_WAY);
+    }
+
+
+
+
     printf("PD initialized!\n"); 
 }
 
